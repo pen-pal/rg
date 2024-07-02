216282d34ed610cf4c253b4c60924ab9e8d6e0ba
pen-pal <unameme@protonmail.com>
2024-07-02 13:01:47 +0545

    update policy

--- s3.tf
+++ s3.tf
@@ -1,3 +1,27 @@
 resource "aws_s3_bucket" "document_bucket" {
   bucket = "${var.stack_name}-document-bucket"
 }
+
+resource "aws_s3_bucket_policy" "document_bucket_policy" {
+  bucket = aws_s3_bucket.document_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = ":"
+        }
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::${aws_s3_bucket.document_bucket.bucket}",
+          "arn:aws:s3:::${aws_s3_bucket.document_bucket.bucket}/*"
+        ]
+      }
+    ]
+  })
+}
